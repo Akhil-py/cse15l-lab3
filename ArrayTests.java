e13366bbc6a2b36b9fe5ea7c9553cd119a875288
Akhil Ram Shankar <ramshankar.akhil@gmail.com>
2023-01-28 14:12:34 -0800

    update

--- ArrayTests.java
+++ ArrayTests.java
@@ -20,4 +20,17 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input3));
     assertArrayEquals(new int[]{ 5, 4, 3, 2, 1 }, ArrayExamples.reversed(input4));
   }
+
+  @Test
+  public void testAverageWithoutLowest(){
+    double[] input5 = { 10.0, 9.0, 10.0 };
+    double[] input6 = { 10, 10, 10 };
+    double[] input7 = { 20 };
+    double[] input8 = { };
+    
+    assertEquals(10.0, ArrayExamples.averageWithoutLowest(input5), 0.001);
+    assertEquals(10.0, ArrayExamples.averageWithoutLowest(input6), 0.001);
+    assertEquals(0.0, ArrayExamples.averageWithoutLowest(input7), 0.001);
+    assertEquals(0.0, ArrayExamples.averageWithoutLowest(input8), 0.001);
+  }
 }
---
+++ ListTests.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ListTests {
+    @Test
+    public void testFilter() {
+        StringChecker cs = new StringChecker {
+    
+        };
+    }
+
+    @Test
+    public void testMerge() {
+
+    }
+}
